084701f3702c64ff083f9fb55977698348c85e3c
btcthirst <btcthirst@gmail.com>
2024-08-06 21:34:47 +0300

    created: generate-keypair.ts/load-keypair.ts-bugs

---
+++ generate-keypair.ts
@@ -0,0 +1,5 @@
+import {Keypair} from "@solana/web3.js"
+const keypair = Keypair.generate()
+console.log(`Public key: ${keypair.publicKey.toBase58()}`)
+console.log(`Private key: ${keypair.secretKey}`)
+console.log(`✅ Finished!`)
