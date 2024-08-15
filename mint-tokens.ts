ca5f5f92d825c0ce1a218b991754a6236ba5a8f1
btcthirst <btcthirst@gmail.com>
2024-08-15 21:52:36 +0300

    added: 4lesson

---
+++ mint-tokens.ts
@@ -0,0 +1,37 @@
+import "dotenv/config";
+import { Connection, Keypair, PublicKey, clusterApiUrl } from "@solana/web3.js";
+import { mintTo } from "@solana/spl-token";
+import { getExplorerLink } from "@solana-developers/helpers";
+
+let privateKey = process.env["SECRET_KEY"];
+if (privateKey === undefined) {
+  console.log("Add SECRET_KEY to .env!");
+  process.exit(1);
+}
+const asArray = Uint8Array.from(JSON.parse(privateKey));
+const sender = Keypair.fromSecretKey(asArray);
+
+const connection = new Connection(clusterApiUrl("devnet"));
+
+// Our token has two decimal places
+const MINOR_UNITS_PER_MAJOR_UNITS = Math.pow(10, 2);
+
+const tokenMintAccount = new PublicKey(
+  "D1ZJnA3q76zh4kiEoHmLHKFdaZsT2cZxZPBwRohhLaDE"
+);
+const recipientAssociatedTokenAccount = new PublicKey(
+    "2Arfn3s16ECF4ZcBAhQq7gUvgEyEnzDJAMBD9GWFn8Ch"
+  );
+  
+  const transactionSignature = await mintTo(
+    connection,
+    sender,
+    tokenMintAccount,
+    recipientAssociatedTokenAccount,
+    sender,
+    10 * MINOR_UNITS_PER_MAJOR_UNITS
+  );
+  
+  const link = getExplorerLink("transaction", transactionSignature, "devnet");
+  
+  console.log(`✅ Success! Mint Token Transaction: ${link}`);
